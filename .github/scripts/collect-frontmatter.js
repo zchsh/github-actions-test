9ead9591e9c1fd0eee8645520df7b0b3ab7a37c6
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-02 10:12:14 -0500

    Update test file with timestamp

--- .github/scripts/collect-frontmatter.js
+++ .github/scripts/collect-frontmatter.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
+const path = require("path");
 
+const filePath = path.join(process.cwd(), ".generated/test-file.txt");
+
+const time = Date.now();
+console.log({ cwd: process.cwd(), filePath, time });
 fs.writeFileSync(
-  ".generated/test-file.txt",
-  "Hello world! This file was written from node.\n"
+  filePath,
+  `Hello world! This file was written from node at ${Date.now()}.\n`
 );
