e557ae299028618e5cea8f19f881685012b2591e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-10 21:34:52 -0500

    Add TODOs to route structure validation

--- .github/scripts/validate-route-structure.js
+++ .github/scripts/validate-route-structure.js
@@ -1,7 +1,6 @@
 const navData = require("../../.generated/docs-navigation.json");
 
 const [rootStack, navDataWithStacks] = validateRouteStructure(navData);
-console.log(JSON.stringify({ rootStack, navDataWithStacks }, null, 2));
 
 function validateRouteStructure(navData, depth = 0) {
   if (navData.length === 0) {
@@ -10,17 +9,7 @@ function validateRouteStructure(navData, depth = 0) {
   // Parse the stacks from the flat, non-nested path children
   const withStacks = navData.map((navNode) => {
     // Handle leaf nodes, which have path values
-    if (typeof navNode.path !== "undefined") {
-      // Empty paths are invalid
-      if (navNode.path === "") {
-        throw new Error(
-          `Empty strings are not valid paths.\n\nNode:\n${JSON.stringify(
-            navNode,
-            null,
-            2
-          )}`
-        );
-      }
+    if (navNode.path) {
       return { __stack: navNode.path.split("/"), ...navNode };
     }
     if (navNode.routes) {
@@ -80,4 +69,42 @@ function validateRouteStructure(navData, depth = 0) {
   }
   console.log({ allPaths, uniqueDirs });
   return [uniqueDirs[0].split("/"), withStacks];
+}
+
+// TODO - throw error if any non-divider node does not have a title
+function collectEmptyTitleErrors(navData) {
+  const errors = [];
+  return errors;
+}
+// TODO - throw error if direct link nodes don't have both { title, href }
+function collectEmptyPathErrors(navData) {
+  const errors = [];
+  return errors;
+}
+
+// TODO - throw error for empty paths
+function findEmptyPaths(navData, depth = 0) {
+  const nodesWithEmptyPaths = navData.reduce((acc, navNode) => {
+    // Handle leaf nodes, which have path values
+    const hasPath = typeof navNode.path !== "undefined";
+    const hasEmptyPath = hasPath && navNode.path === "";
+    if (hasEmptyPath) {
+      acc.push({ node: navNode, depth });
+      throw new Error(
+        `Empty strings are not valid paths.\n\nNode:\n${JSON.stringify(
+          navNode,
+          null,
+          2
+        )}`
+      );
+    }
+    // Handle nested routes
+    if (navNode.routes) {
+      acc.concat(findEmptyPaths(navNode.routes, depth + 1));
+    }
+    // Other nodes don't need to be checked, they don't have
+    // paths or descendants with paths.
+    return acc;
+  }, []);
+  return nodesWithEmptyPaths;
 }
