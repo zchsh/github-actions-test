6a811e6fe39786a439fd7bb631f408b4ce771247
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-10 22:15:18 -0500

    Start cleanup

--- .github/scripts/validate-route-structure.js
+++ .github/scripts/validate-route-structure.js
@@ -1,57 +1,62 @@
 const navData = require("../../.generated/docs-navigation.json");
 
-const [rootStack, navDataWithStacks] = validateRouteStructure(navData);
+validateRouteStructure(navData);
 
-function validateRouteStructure(navData, depth = 0) {
-  if (navData.length === 0) {
-    throw new Error(`Found empty array of navData. Depth: ${depth}`);
+function validateRouteStructure(navData) {
+  const [_rootPath, navDataWithStacks] = validateBranchRoutes(navData);
+  console.log(JSON.stringify(navDataWithStacks, null, 2));
+  return navDataWithStacks;
+}
+
+function validateBranchRoutes(navNodes, depth = 0) {
+  //  In order to be a valid branch, there needs to be at least one navNode.
+  if (navNodes.length === 0) {
+    throw new Error(`Found empty array of navNodes. Depth: ${depth}`);
   }
-  // Parse the stacks from the flat, non-nested path children
-  const withStacks = navData.map((navNode) => {
-    // Handle leaf nodes, which have path values
-    if (navNode.path) {
-      return { __stack: navNode.path.split("/"), ...navNode };
-    }
-    if (navNode.routes) {
-      const [nestedStack, routesWithStacks] = validateRouteStructure(
-        navNode.routes,
-        depth + 1
-      );
-      return { __stack: nestedStack, ...navNode, routes: routesWithStacks };
-    }
-    //  Other nodes aren't relevant, we don't touch them
+  // Augment each navNode with its path __stack
+  const navNodesWithStacks = navNodes.map((navNode) => {
+    // Handle leaf nodes - split their paths into a __stack
+    if (navNode.path) return { ...navNode, __stack: navNode.path.split("/") };
+    // Handle branch nodes - we recurse depth-first here
+    if (navNode.routes) return handleBranchNode(navNode, depth);
+    // Other nodes aren't relevant, we don't touch them
     return navNode;
   });
-  console.log({ withStacks });
-  const routeStacks = withStacks.reduce((acc, navNode) => {
+  // Gather all the path stacks at this level
+  const routeStacks = navNodesWithStacks.reduce((acc, navNode) => {
     // Ignore nodes that don't have a path stack
     if (!navNode.__stack) return acc;
-    // For other nodes, grab their stacks
+    // For other nodes, add their stacks
     return acc.concat([navNode.__stack]);
   }, []);
-  // Ensure that there are no duplicate paths
-  const allPaths = routeStacks.map((s) => s.join("/"));
-  const dupes = allPaths.filter((value, index, self) => {
+  // Ensure that there are no duplicate routes
+  // (for example, a nested route with a particular path,
+  // and a named page at the same level with the same path)
+  const routePaths = routeStacks.map((s) => s.join("/"));
+  const duplicateRoutes = routePaths.filter((value, index, self) => {
     return self.indexOf(value) !== index;
   });
-  if (dupes.length > 0) {
-    throw new Error(`Duplicate routes found:\n\n${JSON.stringify(dupes)}\n`);
+  if (duplicateRoutes.length > 0) {
+    throw new Error(
+      `Duplicate routes found:\n\n${JSON.stringify(duplicateRoutes)}\n`
+    );
   }
-  // Normalize the length of each leaf stack for comparison
-  // with sibling stacks
-  const dirPaths = routeStacks.map((stack) => {
+  // Gather an array of all resolved paths at this level
+  const parentRoutes = routeStacks.map((stack) => {
     // Index leaf nodes will have the same
-    // number of path parts as the current nesting depth
-    if (stack.length === depth) {
-      // For index nodes, the dir path
-      // is identical to the leaf path
+    // number of path parts as the current nesting depth.
+    const isIndexNode = stack.length === depth;
+    if (isIndexNode) {
+      // The "dirPath" for index nodes is
+      // just the original path
       return stack.join("/");
     }
-    // Named leaf nodes will have one more
-    // path part than the current nesting depth
-    if (stack.length === depth + 1) {
-      // For named nodes, the dir path
-      // is sliced from the leaf path
+    // Named leaf nodes, and nested routes,
+    // will have one more path part than the current nesting depth.
+    const isNamedNode = stack.length === depth + 1;
+    if (isNamedNode) {
+      // The "dirPath" for named nodes is
+      // the original path with the last part dropped.
       return stack.slice(0, stack.length - 1).join("/");
     }
     // If we have any other number of parts in the
@@ -60,16 +65,41 @@ function validateRouteStructure(navData, depth = 0) {
       `Invalid path depth. At depth ${depth}, found path "${stack.join("/")}"`
     );
   });
-
-  const uniqueDirs = dirPaths.filter((value, index, self) => {
+  // We expect all routes at any level to share the same parent directory.
+  // In other words, we expect there to be exactly one unique "dirPath"
+  // shared across all the routes at this level.
+  const uniqueParents = parentRoutes.filter((value, index, self) => {
     return self.indexOf(value) === index;
   });
-  if (uniqueDirs.length > 1) {
-    throw new Error(`Found mismatched paths: ${JSON.stringify(uniqueDirs)}`);
+  // We throw an error if we find mismatched paths
+  // that don't share the same parent path.
+  if (uniqueParents.length > 1) {
+    throw new Error(`Found mismatched paths: ${JSON.stringify(uniqueParents)}`);
   }
-  console.log({ allPaths, uniqueDirs });
-  return [uniqueDirs[0].split("/"), withStacks];
+  const path = uniqueParents[0];
+  //  Finally, we return
+  return [path, navNodesWithStacks];
 }
+
+function handleBranchNode(navNode, depth) {
+  // We recurse depth-first here, and we'll throw an error
+  // if the nested routes had structural issues
+  const [path, routesWithStacks] = validateBranchRoutes(
+    navNode.routes,
+    depth + 1
+  );
+  const __stack = path.split("/");
+  return { ...navNode, __stack, routes: routesWithStacks };
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
 
 // TODO - throw error if any non-divider node does not have a title
 function collectEmptyTitleErrors(navData) {
